e7359784752346a18ba92dde72d225d1524c175d
mestiri77 <faroukmestiri77@gmail.com>
2024-05-27 16:54:39 +0100

    client:dev=> session details in progress

--- client/src/pages/sessions/SessionDetails.jsx
+++ client/src/pages/sessions/SessionDetails.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Accordion from 'react-bootstrap/Accordion';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 function SessionDetails() {
+    const [collapse, setCollapse]= useState(false)
     const {id} = useParams()
     console.log(id,"this is the sessionId")
   return (
     <div className='p-4'>
       <div className="d-flex justify-content-end gap-4">
-        <button className="btn btn-outline-dark">Expand All</button>
-        <button className="btn btn-dark">Collapse All</button>
+        <button className="btn btn-outline-dark" onClick={()=>setCollapse(!collapse)}>Expand All</button>
+        <button className="btn btn-dark" onClick={()=>setCollapse(!collapse)}>Collapse All</button>
         <button className="btn btn-success fs-5">+ Add</button>
         
       </div>
-      <Accordion defaultActiveKey={['0']} className='mt-5' alwaysOpen>
+      <Accordion defaultActiveKey={['0']} className='mt-5' >
       {[1,2,3].map((e,i)=>{
         return (
-            <div className='mt-4'>
-      <Accordion.Item eventKey="0" key={i}>
+            <div className='mt-4'key={i}>
+      <Accordion.Item eventKey="0" >
       <Accordion.Header>Week {e}</Accordion.Header>
       <Accordion.Body>
       <ListGroup >
