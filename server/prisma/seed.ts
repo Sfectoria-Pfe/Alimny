47a35a22d4239545b8473d27609669043e0883a6
Malek-Fridhi <fridhiarbia920@gmail.com>
2024-05-28 00:23:47 +0100

    add modal gouvernorat

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -147,6 +147,81 @@ async function main() {
       },
     ],
   });
+  await prisma.Gouvernorat.createMany({
+    data: [
+      {
+        "name": "Ariana"
+      },
+      {
+        "name": "Béja"
+      },
+      {
+        "name": "Ben Arous"
+      },
+      {
+        "name": "Bizerte"
+      },
+      {
+        "name": "Gabès"
+      },
+      {
+        "name": "Gafsa"
+      },
+      {
+        "name": "Jendouba"
+      },
+      {
+        "name": "Kairouan"
+      },
+      {
+        "name": "Kasserine"
+      },
+      {
+        "name": "Kébili"
+      },
+      {
+        "name": "Kef"
+      },
+      {
+        "name": "Mahdia"
+      },
+      {
+        "name": "Manouba"
+      },
+      {
+        "name": "Médenine"
+      },
+      {
+        "name": "Monastir"
+      },
+      {
+        "name": "Nabeul"
+      },
+      {
+        "name": "Sfax"
+      },
+      {
+        "name": "Sidi Bouzid"
+      },
+      {
+        "name": "Siliana"
+      },
+      {
+        "name": "Sousse"
+      },
+      {
+        "name": "Tataouine"
+      },
+      {
+        "name": "Tozeur"
+      },
+      {
+        "name": "Tunis"
+      },
+      {
+        "name": "Zaghouan"
+      }
+    ],});
 
   await prisma.module.createMany({
     data: [
